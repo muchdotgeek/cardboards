f18fd1cee1a45723dbce4c7c75e8b03f999a264e
muchdotgeek <techgeek25@hotmail.com>
2015-11-01 19:14:54 -0800

    lolwut

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 #!/bin/env node
 //  OpenShift sample Node application
-var express = require('express');
+var express = require('express')  
+, app = express()
+, server = require('http').createServer(app)
+, io = require("socket.io").listen(server)
+//... more config and code
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 3000);  
+app.set('ipaddr', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");  
 var fs      = require('fs');
 
 
